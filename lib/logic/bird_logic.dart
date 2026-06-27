58bd82a55739d90dc137c272b082c78da0b33f86
Fred <fawomack@gmail.com>
2026-06-27 14:20:59 -0400

    cleaning up formating

--- lib/logic/bird_logic.dart
+++ lib/logic/bird_logic.dart
@@ -3,21 +3,15 @@ import '../constants.dart';
 
 class BirdLogic {
   double yPosition = 100.0;// initial position
-  
-
   double velocity = 0.0;  // Initial velocity (how fast it's falling)
 
   // This method will be called every frame
   void update(double dt) {
-    // 1. Apply simple gravity: increase velocity over time
-    velocity += GameConfig.gravity * dt;
-    
-    // 2. Update position based on velocity
-    yPosition += velocity * dt;
+    velocity += GameConfig.gravity * dt;    // 1. Apply simple gravity: increase velocity over time
+    yPosition += velocity * dt;    // 2. Update position based on velocity
   }
 
   void jump() {
-    // Negative velocity moves the bird up
-    velocity = GameConfig.jumpStrength;
+    velocity = GameConfig.jumpStrength;    // Negative velocity moves the bird up
   }
 }
