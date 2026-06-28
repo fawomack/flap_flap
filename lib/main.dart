5261268ff8051fd98efd5641547d641ad64bced0
Fred <fawomack@gmail.com>
2026-06-27 21:05:30 -0400

    can only be played landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flame/game.dart';
+import 'package:flutter/services.dart'; // Import this for SystemChrome
 import 'package:flutter/material.dart';
 import 'screens/game_screen.dart';
 
-void main() {
+void main() async{
   // Initialize the Flutter widgets
   WidgetsFlutterBinding.ensureInitialized();
   
+// Force the app into Landscape mode
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
   // Launch the GameScreen directly
   // Note: There is NO 'MyApp' class here anymore!
   runApp(GameWidget(game: GameScreen()));
